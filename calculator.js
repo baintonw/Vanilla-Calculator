c580e0dfb6fe3cf39aba4a64cae359861814c41c
Will Bainton <baintonw@gmail.com>
2019-11-11 16:59:30 -0500

    calc evaluates equation but does not stack operands

--- calculator.js
+++ calculator.js
@@ -1,12 +1,13 @@
 const buttons = [...document.querySelectorAll('button')];
 const screen = document.querySelector('#screen');
 const ac = document.querySelector('[data-button = "AC"]');
+const equals = document.querySelector('#equals-btn')
 const sound = document.querySelector('audio');
 
 
 let numberArray = [];
 let number = 0;
-screen.innerText = 0;
+
 
 let first = true
 let operand;
@@ -18,6 +19,7 @@ let y = ''
 let equation;
 
 const math = {
+
   '+': function(x, y) { return x + y },
   '-': function(x, y) { return x - y },
   'x': function(x, y) { return x * y },
@@ -46,28 +48,42 @@ function calculate(e){
     console.log('y', y)
 
 
-  } else if(!first) {
-    numberArray.push(e.target.innerText)
-    number = parseFloat(numberArray.join(''))
-
-    y = number
-    console.log('y', y)
   }
 
   if(e.target.dataset.button === 'operand') {
-    // console.log('LIVE ONE!', 'operand', e.target.innerHTML)
-    // if(!first) {
-    //   math[]
-    // }
-    first = false
-    screen.innerText = x
-    numberArray = []
-    number = 0
+    console.log('LIVE ONE!', 'operand', e.target.innerHTML)
+    if(x&&y) {
+      x = math[operand](x, y)
+      numberArray = []
+      number = 0
+    } else {
+      numberArray = []
+      number = 0
+      operand = e.target.innerHTML
+      first = false
+      screen.innerHTML = x
+    }
+
+
+
+
     // equation = math[e.target.innerHTML]
     //
     // console.log(equation(4, 2))
   }
 
+}
+
+function evaluate(e) {
+  console.log('evaluating!')
+  console.log('equals:', math[operand](x,y))
+  number = math[operand](x,y)
+  screen.innerHTML = number
+  x = ''
+  y = ''
+  numberArray = []
+  number = 0;
+  first = true
 }
 
 //once x is set and I hit the operand, how can I set Y?
@@ -96,3 +112,4 @@ buttons.forEach(button => {
 })
 
 ac.addEventListener('click', clear)
+equals.addEventListener('click', evaluate)
