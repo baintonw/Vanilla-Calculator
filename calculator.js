ee872d11a18d4fbadf08bf92c0337e9cbb6e00f4
Will Bainton <baintonw@gmail.com>
2019-11-14 16:16:17 -0500

    cleared unnecssary comments and console logs

--- calculator.js
+++ calculator.js
@@ -8,7 +8,6 @@ const sound = document.querySelector('audio');
 let numberArray = [];
 let number = 0;
 let prevNum = ''
-// let screen.innerText = number
 
 
 let first = true;
@@ -18,7 +17,6 @@ let operand;
 
 let x = '';
 let y = '';
-// let equation = `x ${operand} y`
 
 const math = {
 
@@ -29,10 +27,8 @@ const math = {
 
 };
 
-//calculate (fill the operand in the math object key, and set the value of x and y) and then evaluate (upon hitting '=' evaluate the stored information)
 
 
-//displays number typed in on screen
 function show(e, x, y) {
   if(first && x) {
     screen.innerText = x
@@ -54,19 +50,19 @@ function calculate(e) {
   }
 
   if(e.target.dataset.button === 'operand') {
-    debugger
+    
     if(!first && prevNum) {
       numberArray = []
       operand = e.target.innerHTML
       first = false
-      debugger
+      
     }else if(!first) {
       x = math[operand](x, y)
       show(e, x, y)
       numberArray = []
-      debugger
+      
     } else if(x) {
-      debugger
+      
       numberArray = []
       operand = e.target.innerHTML
       show(e, x, y)
@@ -84,15 +80,12 @@ function evaluate(e) {
     screen.innerText = number
     numberArray = []
     number = 0
-    // first = true
     console.log('this is the value of x:', x, 'number:', number , first)
   }
 
 }
 
-//once x is set and I hit the operand, how can I set Y?
 
-//clears display and numberArray (the first value)
 function clear(e){
   console.log('cleared!')
   numberArray = []
@@ -103,18 +96,8 @@ function clear(e){
   first = true
 }
 
-//should i make a math object that takes in the operand string as a key and returns a function?
-
-
-
-
-
 buttons.forEach(button => {
-
-
   button.addEventListener('click', calculate)
-
-
 })
 
 ac.addEventListener('click', clear)
